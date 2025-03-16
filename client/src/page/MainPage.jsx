7fbb31eeffb8aa9a8823a237d3ee3dbe06107658
rashmikaz <phrashmika@gmail.com>
2025-03-17 04:04:51 +0530

    Target Currency

--- client/src/page/MainPage.jsx
+++ client/src/page/MainPage.jsx
@@ -49,6 +49,24 @@ const MainPage = () => {
               </select>
             </div>
 
+            <div className="mb-4">
+              <label
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                htmlFor="sourceCurrency"
+              >
+                Target Currency
+              </label>
+
+              <select
+                className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500"
+                name="sourceCurrency"
+                id=""
+              >
+                <option value="">Select target currency</option>{" "}
+           
+              </select>
+            </div>
+
           </form>
         </section>
       </div>
