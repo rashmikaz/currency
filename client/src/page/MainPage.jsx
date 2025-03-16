aaede52538aca58e9ff65f838e0cf5a9c77aa597
rashmikaz <phrashmika@gmail.com>
2025-03-17 03:48:00 +0530

    para

--- client/src/page/MainPage.jsx
+++ client/src/page/MainPage.jsx
@@ -6,6 +6,13 @@ const MainPage = () => {
       <h1 className="lg:mx-32  text-5xl font-black flex items-center justify-normal text-green-500">
         Convert Your Currencies Today
       </h1>
+      <p className="lg:mx-32 font-sm opacity-40 py-6">
+        Welcome to "Convert Your Currencies Today"! This application allows you
+        to easily convert currencies based on the latest exchange rates. Whether
+        you're planning a trip, managing your finances, or simply curious about
+        the value of your money in different currencies, this tool is here to
+        help.
+      </p>
        
       
 
