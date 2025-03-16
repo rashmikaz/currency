d9ce5f2fe43bb4d211f001bf66416f19ad00ed32
rashmikaz <phrashmika@gmail.com>
2025-03-17 04:06:49 +0530

    Amount in source currency

--- client/src/page/MainPage.jsx
+++ client/src/page/MainPage.jsx
@@ -67,6 +67,21 @@ const MainPage = () => {
               </select>
             </div>
 
+            <div className="mb-4">
+              <label
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                htmlFor="date"
+              >
+                Amount in source currency
+              </label>
+              <input
+                required
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500"
+                type="text"
+                
+              />
+            </div>
+
           </form>
         </section>
       </div>
