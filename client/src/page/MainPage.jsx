f96cfd488e1e02dcfb7ceb51258fa08bd510a0b2
rashmikaz <phrashmika@gmail.com>
2025-03-17 03:47:35 +0530

    heading

--- client/src/page/MainPage.jsx
+++ client/src/page/MainPage.jsx
@@ -3,6 +3,9 @@ import React from 'react'
 const MainPage = () => {
   return (
     <div>
+      <h1 className="lg:mx-32  text-5xl font-black flex items-center justify-normal text-green-500">
+        Convert Your Currencies Today
+      </h1>
        
       
 
